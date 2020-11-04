3773c285a90c20a8c9641ecf44bf64d6bebb4d4e
Иван Туманов <tumanov99923@hotmail.com>
2020-11-04 19:47:36 +0300

    Добавьте файлы проекта.

---
+++ TasksCSharp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TasksCSharp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int year = 365;
+            const int month = 30;
+
+            // Вводим количество дней
+            Console.WriteLine("Write days: ");
+            int Day = int.Parse(Console.ReadLine());
+
+            // Вычислим и выведем количество лет, сохранив остаток
+            Console.WriteLine("Лет: " + Day / year);
+            Day = Day % year;
+
+            Console.WriteLine("Месяцев: " + Day / month);
+            Day = Day % month;
+
+            Console.WriteLine("Дней: " + Day);
+
+            Console.ReadKey();
+        }
+    }
+}
